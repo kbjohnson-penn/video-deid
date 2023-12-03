a691e4cff26a27cfeacf6592f5be8a56867d3424
Sriharsha Mopidevi <31341894+sriharshamvs@users.noreply.github.com>
2023-12-03 10:55:58 -0500

    fix: load frames into generator & show logs

--- video-deid.py
+++ video-deid.py
@@ -1,70 +1,24 @@
 import cv2
 import numpy as np
-import ast  # To convert string representation of list to actual list
-import subprocess
+import ast
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def read_keypoints(file_path):
-    keypoints = []
-    with open(file_path, 'r') as file:
-        for line in file:
-            # Convert string representation of list to an actual list
-            keypoints.append(ast.literal_eval(line.strip()))
-    return keypoints
-
-
-def load_video(file_path):
-    cap = cv2.VideoCapture(file_path)
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
-
-    frames = []
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        frames.append(frame)
-    cap.release()
-    return frames, width, height, frame_rate
-
-
-def blur_face_rectangle(frame, keypoints_list):
-    if len(keypoints_list) == 17:
-        persons_keypoints = [keypoints_list]
-    else:
-        persons_keypoints = keypoints_list
-
-    for keypoints in persons_keypoints:
-        face_keypoints = [kp for kp in keypoints[:5]
-                          if isinstance(kp, list) and len(kp) == 2]
-        filtered_keypoints = [kp for kp in face_keypoints if not (
-            kp[0] == 0.0 and kp[1] == 0.0)]
-
-        if filtered_keypoints:
-            visible_face_keypoints = np.array(filtered_keypoints)
-
-            if len(filtered_keypoints) >= 2:
-                # Calculate bounding box for multiple keypoints
-                x_min = int(min(visible_face_keypoints[:, 0]))
-                y_min = int(min(visible_face_keypoints[:, 1]))
-                x_max = int(max(visible_face_keypoints[:, 0]))
-                y_max = int(max(visible_face_keypoints[:, 1]))
-            else:
-                # Use a default size for the blur area when only one keypoint is available
-                default_size = 100  # This can be adjusted
-                kp = visible_face_keypoints[0]
-                x_min = max(0, int(kp[0] - default_size / 2))
-                y_min = max(0, int(kp[1] - default_size / 2))
-                x_max = min(frame.shape[1], int(kp[0] + default_size / 2))
-                y_max = min(frame.shape[0], int(kp[1] + default_size / 2))
-
-            face_region = frame[y_min:y_max, x_min:x_max]
-            if face_region.size > 0:
-                blurred_face = cv2.GaussianBlur(face_region, (99, 99), 30)
-                frame[y_min:y_max, x_min:x_max] = blurred_face
-
-    return frame
+    try:
+        with open(file_path, 'r') as file:
+            for line in file:
+                yield ast.literal_eval(line.strip())
+    except FileNotFoundError:
+        logging.error(f"Keypoints file not found: {file_path}")
+        raise
+    except Exception as e:
+        logging.error(f"Error reading keypoints file: {e}")
+        raise
 
 
 def blur_face_circle(frame, keypoints_list):
@@ -111,40 +65,72 @@ def blur_face_circle(frame, keypoints_list):
     return frame
 
 
-def join_frames(frames, width, height, frame_rate, output_path):
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(output_path, fourcc,
-                          frame_rate, (width, height))
+def process_video(video_path, keypoints_file_path, output_path):
+    logging.info(f"Starting video processing for {video_path}")
 
-    for frame in frames:
-        out.write(frame)
+    try:
+        cap = cv2.VideoCapture(video_path)
+        if not cap.isOpened():
+            logging.error(f"Unable to open video file: {video_path}")
+            return
 
-    out.release()
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter(output_path, fourcc, frame_rate, (width, height))
+
+        keypoints_generator = read_keypoints(keypoints_file_path)
+        frame_count = 0
+        update_interval = 100  # Adjust as needed
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                logging.info("No more frames to read from video.")
+                break
+
+            frame_count += 1
+            if frame_count % update_interval == 0:
+                percentage = (frame_count / total_frames) * 100
+                logging.info(
+                    f"Processing frame {frame_count}/{total_frames} ({percentage:.2f}%)")
+
+            try:
+                keypoints = next(keypoints_generator)
+            except StopIteration:
+                logging.info("No more keypoints available.")
+                break
+            except Exception as e:
+                logging.error(
+                    f"Error processing keypoints for frame {frame_count}: {e}")
+                break
+
+            try:
+                processed_frame = blur_face_circle(frame, keypoints)
+                out.write(processed_frame)
+            except Exception as e:
+                logging.error(
+                    f"Error applying blur to frame {frame_count}: {e}")
+                break
+
+    except Exception as e:
+        logging.error(f"Unexpected error: {e}")
+    finally:
+        if cap:
+            cap.release()
+        if out:
+            out.release()
+        logging.info("Released video resources.")
+
+    logging.info(f"Video processing completed. Output saved to {output_path}")
 
 
-def merge_audio(video_path, audio_path, output_path):
-    cmd = ['ffmpeg', '-i', video_path, '-i', audio_path, '-c:v',
-           'copy', '-c:a', 'aac', '-strict', 'experimental', output_path]
-    subprocess.run(cmd)
+# Usage
+video_path = '/Users/mopidevi/Downloads/CSI_3.10.18_Sweeney_03.mp4'
+keypoints_file_path = '/Users/mopidevi/Workspace/projects/video-deid/keypoints_xy.txt'
+output_path = '/Users/mopidevi/Workspace/projects/video-deid/output_1.mp4'
 
-
-# Path to Keypoints and Video
-video_path = '/Users/mopidevi/Workspace/projects/video-deid/CSI_03.02.18_Trexler_01_TRIMMED.mp4'
-keypoints_file_path = '/Users/mopidevi/Workspace/projects/video-deid/models/yolov8m-pose/keypoints_xy.txt'
-output_path = '/Users/mopidevi/Workspace/projects/video-deid/models/yolov8m-pose/deid-cir-yolov8m-pose.mp4'
-
-video_keypoints = read_keypoints(keypoints_file_path)
-video_frames, width, height, frame_rate = load_video(video_path)
-
-
-if len(video_keypoints) == len(video_frames):
-    processed_frames = []
-    for idx in range(len(video_keypoints)):
-        processed_frame = blur_face_circle(
-            video_frames[idx], video_keypoints[idx])
-        processed_frames.append(processed_frame)
-
-    subprocess.run(['ffmpeg', '-i', video_path, '-q:a',
-                   '0', '-map', 'a', 'original-audio.mp3'])
-    join_frames(processed_frames, width, height, frame_rate, 'video-deid.mp4')
-    merge_audio('video-deid.mp4', 'original-audio.mp3', output_path)
+process_video(video_path, keypoints_file_path, output_path)
