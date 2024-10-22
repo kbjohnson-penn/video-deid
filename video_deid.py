ed264e449c585fbfc75b25551bc0011e18b69c6b
Sriharsha Mopidevi <sriharshamvs@gmail.com>
2024-10-21 21:59:59 -0400

    enhancement: add new args to completely deid video

--- video_deid.py
+++ video_deid.py
@@ -5,12 +5,13 @@ import tempfile
 from helpers.utils import create_run_directory_and_paths, setup_logging, load_dataframe_from_csv, interpolate_and_sort_df
 from helpers.video_blur import process_video
 from helpers.audio import combine_audio_video
+from helpers.complete_deid import blur_video, process_blurred_video
 
 
 def parse_arguments():
     """Parse command line arguments."""
     parser = argparse.ArgumentParser(
-        description='Process video and apply blur to faces.')
+        description='Process video and apply blur to faces or completely de-identify the video.')
     parser.add_argument('--video', required=True,
                         help='Path to the input video.')
     parser.add_argument('--keypoints_csv', required=True,
@@ -18,8 +19,12 @@ def parse_arguments():
     parser.add_argument('--output', required=True,
                         help='Path to the output video.')
     parser.add_argument('--log', action='store_true', help='Enable logging.')
-    parser.add_argument("--progress", action="store_true",
-                        help="Show progress bar")
+    parser.add_argument('--progress', action='store_true',
+                        help='Show progress bar')
+    parser.add_argument('--deid', action='store_true',
+                        help='Completely de-identify the video (blur entire video).')
+    parser.add_argument('--notemp', action='store_true',
+                        help='Do not use temporary files, save all files in the runs directory.')
     return parser.parse_args()
 
 
@@ -53,21 +58,39 @@ def interpolate_keypoints(keypoints_dataframe, output_path):
     return interpolated_keypoints_df
 
 
-def process_video_with_audio(args, keypoints_dataframe, interpolated_keypoints_df, kalman_filtered_csv_path):
+def process_video_with_audio(args, keypoints_dataframe, interpolated_keypoints_df, kalman_filtered_csv_path, output_video_path):
     """Process the video and combine audio."""
-    with tempfile.NamedTemporaryFile(suffix=".mp4", delete=False) as temp_file:
-        try:
-            logging.info('Processing video.')
-            process_video(args.video, keypoints_dataframe, interpolated_keypoints_df,
-                          kalman_filtered_csv_path, temp_file.name, args.progress)
-            logging.info('Combining audio and video.')
-            combine_audio_video(args.video, temp_file.name, args.output)
-        except Exception as e:
-            logging.error(f"An error occurred during video processing: {e}")
-        finally:
-            if os.path.exists(temp_file.name):
-                os.remove(temp_file.name)
-                logging.info('Temporary video file removed.')
+    try:
+        logging.info('Processing video.')
+        process_video(args.video, keypoints_dataframe, interpolated_keypoints_df,
+                      kalman_filtered_csv_path, output_video_path, args.progress)
+        logging.info('Combining audio and video.')
+        combine_audio_video(args.video, output_video_path, args.output)
+    except Exception as e:
+        logging.error(f"An error occurred during video processing: {e}")
+
+
+def deidentify_video(args, keypoints_dataframe, output_video_path):
+    """Completely de-identify the video by blurring all frames."""
+    try:
+        logging.info(
+            f"Starting complete de-identification for video -- {args.video}.")
+        # Blur the entire video
+        blur_video(args.video, output_video_path, args.progress)
+        logging.info("Completed video blurring.")
+
+        if keypoints_dataframe is not None:
+            logging.info("Processing blurred video to overlay keypoints.")
+            process_blurred_video(output_video_path,
+                                  keypoints_dataframe, args.output, args.progress)
+        else:
+            logging.info(
+                f"Saving fully blurred video without keypoint annotations to {args.output}.")
+            os.rename(output_video_path, args.output)
+
+        logging.info(f"Completed processing all frames -- {args.video}.")
+    except Exception as e:
+        logging.error(f"An error occurred during de-identification: {e}")
 
 
 def main():
@@ -88,9 +111,24 @@ def main():
     interpolated_keypoints_df = interpolate_keypoints(
         keypoints_dataframe, paths['interpolated_csv'])
 
-    # Process the video and combine audio
-    process_video_with_audio(args, keypoints_dataframe,
-                             interpolated_keypoints_df, paths['kalman_filtered_csv'])
+    # Determine output paths based on --notemp flag
+    if args.notemp:
+        output_video_path = os.path.join(
+            paths['run_directory'], os.path.basename(args.output))
+        kalman_filtered_csv_path = os.path.join(
+            paths['run_directory'], 'kalman_filtered_keypoints.csv')
+    else:
+        with tempfile.NamedTemporaryFile(suffix=".mp4", delete=False) as temp_file:
+            output_video_path = temp_file.name
+        kalman_filtered_csv_path = paths['kalman_filtered_csv']
+
+    if args.deid:
+        # Completely de-identify the video
+        deidentify_video(args, keypoints_dataframe, output_video_path)
+    else:
+        # Process the video and combine audio
+        process_video_with_audio(args, keypoints_dataframe,
+                                 interpolated_keypoints_df, kalman_filtered_csv_path, output_video_path)
 
     logging.info('Finished processing video.')
 
